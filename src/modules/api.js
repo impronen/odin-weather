1feaa619b753a9f593a9ec7ed6f704bd731e9666
impronen <impronen@gmail.com>
2023-04-06 09:24:58 +0300

    API fix

--- src/modules/api.js
+++ src/modules/api.js
@@ -8,7 +8,7 @@ const fetchWeather = async (location) => {
     }
     try {
       const response = await fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=47db8690ea3e4433803123706232203&q=${location}&days=6`
+        `https://api.weatherapi.com/v1/forecast.json?key=15e42e0620b649a8ae961742230604&q=${location}&days=6`
       );
       const responseData = await response.json();
       console.log(responseData);
